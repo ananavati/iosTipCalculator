d2809d58a086cb5565bda4c6b033cf32029fa01d
Arpan Nanavati <nanavatiarpan@gmail.com>
2014-02-18 00:33:50 -0800

    first pass at calculating tips from slider and stepper

--- MakeTip/MakeTipViewController.m
+++ MakeTip/MakeTipViewController.m
@@ -9,6 +9,16 @@
 #import "MakeTipViewController.h"
 
 @interface MakeTipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalAmountLabel;
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+@property (weak, nonatomic) IBOutlet UIStepper *tipStepper;
+
+- (IBAction)onTap:(id)sender;
+- (IBAction)onSwipeUp:(id)sender;
+- (void)updateAllValues;
 
 @end
 
@@ -26,7 +36,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self.billAmountTextField becomeFirstResponder];
+    [self updateAllValues];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +46,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    [self updateAllValues];
+}
+
+- (IBAction)onSwipeUp:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onSliderChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    
+    int tipPercent = (int)slider.value;
+    
+    self.tipPercentLabel.text = [NSString stringWithFormat:@"(%d%%)", tipPercent];
+}
+
+- (void)updateAllValues {
+    float billAmount = [self.billAmountTextField.text floatValue];
+    int tipStepValue = (int)self.tipStepper.value;
+    int tipSliderValue = (int)self.tipSlider.value;
+    int tipPercent = tipStepValue > 0 ? tipStepValue + tipSliderValue : tipSliderValue;
+    
+    float tipAmount = billAmount * tipPercent/100;
+    float totalAmount = tipAmount + billAmount;
+//    NSLog(@"%d%%", tipAmount);
+    
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalAmountLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+    self.tipPercentLabel.text = [NSString stringWithFormat:@"%d%%", tipPercent];
+}
 @end
