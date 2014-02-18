35d345fffc97c517513c67758049adf4e568a400
Arpan Nanavati <nanavatiarpan@gmail.com>
2014-02-17 23:52:42 -0800

    first commit

--- MakeTip/AppDelegate.m
+++ MakeTip/AppDelegate.m
@@ -13,7 +13,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    MakeTipViewController *view = [[MakeTipViewController alloc] init];
+    self.window.rootViewController = view;
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ MakeTip/MakeTipViewController.h
@@ -0,0 +1,13 @@
+//
+//  MakeTipViewController.h
+//  MakeTip
+//
+//  Created by Arpan Nanavati on 2/17/14.
+//  Copyright (c) 2014 Arpan Nanavati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MakeTipViewController : UIViewController
+
+@end
