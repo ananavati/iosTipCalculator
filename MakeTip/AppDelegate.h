43d2deaffd09b33250f6e1159fe56735ca8ef7a5
Arpan Nanavati <nanavatiarpan@gmail.com>
2014-02-17 22:38:18 -0800

    Initial Commit

---
+++ MakeTip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MakeTip
+//
+//  Created by Arpan Nanavati on 2/17/14.
+//  Copyright (c) 2014 Arpan Nanavati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
